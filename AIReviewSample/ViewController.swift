f926d4a943d8b93e009bdd3b44ae4e8ab9729058
fujisawa_yum <fujisawa_yum@msoft.co.jp>
2024-06-27 06:29:30 +0900

    Create project

---
+++ AIReviewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIReviewSample
+//
+//  Created by Developer on 2024/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
