f9ca834c1be080e3cfa9217abe9ad75e545e9459
fujisawa_yum <fujisawa_yum@msoft.co.jp>
2024-07-25 00:50:03 +0900

    Labelを追加（誤字テスト）

--- AIReviewSample/ViewController.swift
+++ AIReviewSample/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // 小文字大文字の誤字（Lとl）
+    @IBOutlet private weak var label1: UILabel!
+    @IBOutlet private weak var Label2: UILabel!
+    @IBOutlet private weak var label3: UILabel!
+    @IBOutlet private weak var label4: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 単語の誤字（英語、日本語、略語）
+        let testString1 = "テスト"
+        let testStrung2 = "テスt"
+        let tetString3 = "テスト"
+        
+        label1.text = testString1
+        Label2.text = testStrung2
+        label3.text = tetString3
     }
 
 
